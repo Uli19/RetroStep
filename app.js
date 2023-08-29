1f5e3a2de208b3f6448e26e67edb9d3a9cb2fd04
Axel <axesan917@gmail.com>
2023-08-29 17:13:55 -0600

    Mejora el estilo de botón y reparación del primer click del menu hamburguesa

--- app.js
+++ app.js
@@ -2,12 +2,7 @@ let menu = document.getElementById("menu");
 let sideMenu = document.getElementById("side-menu");
 let x = document.getElementById("x");
 menu.addEventListener("click", function() {
-    if (sideMenu.style.left === "-250px") {
-        sideMenu.style.left = "0";
-        
-    } else {
-        sideMenu.style.left = "-250px";
-    }
+   sideMenu.style.left ="0px";
 
 });
 x.addEventListener("click",function(){
