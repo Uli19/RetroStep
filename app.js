4478f51ebd44478025b62eef694716b14d7bf958
Axel <axesan917@gmail.com>
2023-08-29 18:19:32 -0600

    Generación automatica de botones borrar y editar al generar una fila en la tabla

--- app.js
+++ app.js
@@ -8,3 +8,32 @@ menu.addEventListener("click", function() {
 x.addEventListener("click",function(){
     sideMenu.style.left= "-250px";
 });
+
+let userTable = document.getElementById("userTable");
+
+// Función para agregar una nueva fila con botones de Editar y Borrar
+function agregarFila(id, rol, nombre) {
+    let newRow = document.createElement("tr");
+    newRow.innerHTML = `
+        <td>${id}</td>
+        <td>${rol}</td>
+        <td>${nombre}</td>
+        <td><button class="EditarBtn">Editar</button></td>
+        <td><button class="EliminarBtn">Borrar</button></td>
+    `;
+    userTable.appendChild(newRow);
+}
+
+// Llamada a la función para agregar una fila de ejemplo
+agregarFila("1", "Admin", "Juan Pérez");
+
+// Event listener para botones de Editar y Borrar (puedes manejar la lógica según tus necesidades)
+userTable.addEventListener("click", function(event) {
+    if (event.target.classList.contains("EditarBtn")) {
+        // Lógica para editar
+        console.log("Editar fila");
+    } else if (event.target.classList.contains("EliminarBtn")) {
+        // Lógica para borrar
+        console.log("Borrar fila");
+    }
+});
